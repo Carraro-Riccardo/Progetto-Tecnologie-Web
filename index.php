3e7a40224ca3e892334f14847633e4315276bea7
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2023-11-27 09:31:17 +0100

    Initial setup

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" media="screen">
+    <link rel="stylesheet" href="print.css" media="print">
+    <link rel="stylesheet" href="phone.css" media="screen and max-width:600px">
+    <title>Document</title>
+</head>
+<body>
+    ciao
+</body>
+</html>
