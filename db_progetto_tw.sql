44e9f0ed72d9bdb82226e8fd08866e4562191c34
Carraro-Riccardo <77143275+Carraro-Riccardo@users.noreply.github.com>
2024-01-06 10:38:10 +0100

    updated database

--- db_progetto_tw.sql
+++ db_progetto_tw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Dic 24, 2023 alle 17:24
+-- Creato il: Gen 06, 2024 alle 10:35
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -22,14 +22,12 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `db_progetto_tw` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `db_progetto_tw`;
-
 -- --------------------------------------------------------
 
 --
 -- Struttura della tabella `abbonamenti`
 --
 
-DROP TABLE IF EXISTS `abbonamenti`;
 CREATE TABLE `abbonamenti` (
   `id` int(11) NOT NULL,
   `nome` varchar(255) DEFAULT NULL,
@@ -38,17 +36,20 @@ CREATE TABLE `abbonamenti` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Svuota la tabella prima dell'inserimento `abbonamenti`
+-- Dump dei dati per la tabella `abbonamenti`
 --
 
-TRUNCATE TABLE `abbonamenti`;
+INSERT INTO `abbonamenti` (`id`, `nome`, `durata`, `costo`) VALUES
+(1, 'Mensile', 30, 50.00),
+(2, 'Trimestrale', 90, 140.00),
+(3, 'Annuale', 365, 500.00);
+
 -- --------------------------------------------------------
 
 --
 -- Struttura della tabella `allenatori`
 --
 
-DROP TABLE IF EXISTS `allenatori`;
 CREATE TABLE `allenatori` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) DEFAULT NULL,
@@ -58,17 +59,19 @@ CREATE TABLE `allenatori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Svuota la tabella prima dell'inserimento `allenatori`
+-- Dump dei dati per la tabella `allenatori`
 --
 
-TRUNCATE TABLE `allenatori`;
+INSERT INTO `allenatori` (`id`, `nome`, `cognome`, `data_di_nascita`, `descrizione`) VALUES
+(1, 'nomeA1', 'cognomeA1', '2000-01-01', ' universalmente riconosciuto che un lettore che osserva il layout di una pagina viene distratto dal contenuto testuale se questo è leggibile. Lo scopo dell’utilizzo del Lorem Ipsum è che offre una normale distribuzione delle lettere (al contrario di quant'),
+(2, 'nomeA2', 'cognomeA2', '2000-01-02', ' universalmente riconosciuto che un lettore che osserva il layout di una pagina viene distratto dal contenuto testuale se questo è leggibile. Lo scopo dell’utilizzo del Lorem Ipsum è che offre una normale distribuzione delle lettere (al contrario di quant');
+
 -- --------------------------------------------------------
 
 --
 -- Struttura della tabella `esercizi`
 --
 
-DROP TABLE IF EXISTS `esercizi`;
 CREATE TABLE `esercizi` (
   `id` int(11) NOT NULL,
   `nome` varchar(255) DEFAULT NULL,
@@ -77,34 +80,51 @@ CREATE TABLE `esercizi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Svuota la tabella prima dell'inserimento `esercizi`
+-- Dump dei dati per la tabella `esercizi`
 --
 
-TRUNCATE TABLE `esercizi`;
+INSERT INTO `esercizi` (`id`, `nome`, `descrizione`, `id_macchinario`) VALUES
+(1, 'Esercizio1', 'Descrizione1', 1),
+(2, 'Esercizio2', 'Descrizione2', 2),
+(3, 'Esercizio3', 'Descrizione3', 3),
+(4, 'Esercizio4', 'Descrizione4', 4),
+(5, 'Esercizio5', 'Descrizione5', 5),
+(6, 'Esercizio6', 'Descrizione6', 6),
+(7, 'Esercizio7', 'Descrizione7', 7),
+(8, 'Esercizio8', 'Descrizione8', 8),
+(9, 'Esercizio9', 'Descrizione9', 9),
+(10, 'Esercizio10', 'Descrizione10', 10);
+
 -- --------------------------------------------------------
 
 --
 -- Struttura della tabella `gruppimuscolari`
 --
 
-DROP TABLE IF EXISTS `gruppimuscolari`;
 CREATE TABLE `gruppimuscolari` (
   `ID` int(11) NOT NULL,
   `gruppoMuscolare` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Svuota la tabella prima dell'inserimento `gruppimuscolari`
+-- Dump dei dati per la tabella `gruppimuscolari`
 --
 
-TRUNCATE TABLE `gruppimuscolari`;
+INSERT INTO `gruppimuscolari` (`ID`, `gruppoMuscolare`) VALUES
+(1, 'Schiena'),
+(2, 'Spalle'),
+(3, 'Bicipiti'),
+(4, 'Tricipiti'),
+(5, 'Petto'),
+(6, 'Gambe'),
+(7, 'Addominali');
+
 -- --------------------------------------------------------
 
 --
 -- Struttura della tabella `macchinari`
 --
 
-DROP TABLE IF EXISTS `macchinari`;
 CREATE TABLE `macchinari` (
   `id` int(11) NOT NULL,
   `nome` varchar(30) DEFAULT NULL,
@@ -113,18 +133,48 @@ CREATE TABLE `macchinari` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Svuota la tabella prima dell'inserimento `macchinari`
+-- Dump dei dati per la tabella `macchinari`
 --
 
-TRUNCATE TABLE `macchinari`;
+INSERT INTO `macchinari` (`id`, `nome`, `dataDiAcquisto`, `gruppoMuscolare`) VALUES
+(1, 'Macchina1', '2022-01-01', 1),
+(2, 'Macchina2', '2022-01-02', 2),
+(3, 'Macchina3', '2022-01-03', 3),
+(4, 'Macchina4', '2022-01-04', 4),
+(5, 'Macchina5', '2022-01-05', 5),
+(6, 'Macchina6', '2022-01-06', 6),
+(7, 'Macchina7', '2022-01-07', 7),
+(8, 'Macchina8', '2022-01-08', 1),
+(9, 'Macchina9', '2022-01-09', 2),
+(10, 'Macchina10', '2022-01-10', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `scheda`
+--
+
+CREATE TABLE `scheda` (
+  `id_scheda` int(11) NOT NULL,
+  `id_allenatore` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `scheda`
+--
+
+INSERT INTO `scheda` (`id_scheda`, `id_allenatore`) VALUES
+(1, 1),
+(2, 2);
+
 -- --------------------------------------------------------
 
 --
 -- Struttura della tabella `schede_esercizi`
 --
 
-DROP TABLE IF EXISTS `schede_esercizi`;
 CREATE TABLE `schede_esercizi` (
+  `id` int(11) NOT NULL,
   `id_scheda` int(11) DEFAULT NULL,
   `id_esercizio` int(11) DEFAULT NULL,
   `giorno_settimana` enum('lunedi','martedi','mercoledi','giovedi','venerdi','sabato','domenica') DEFAULT NULL,
@@ -133,56 +183,71 @@ CREATE TABLE `schede_esercizi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Svuota la tabella prima dell'inserimento `schede_esercizi`
+-- Dump dei dati per la tabella `schede_esercizi`
 --
 
-TRUNCATE TABLE `schede_esercizi`;
+INSERT INTO `schede_esercizi` (`id`, `id_scheda`, `id_esercizio`, `giorno_settimana`, `numero_set`, `numero_ripetizioni`) VALUES
+(1, 1, 1, 'lunedi', 3, 10),
+(2, 1, 2, 'lunedi', 3, 10),
+(3, 1, 3, 'mercoledi', 3, 10),
+(4, 1, 4, 'mercoledi', 3, 10),
+(5, 1, 5, 'venerdi', 3, 10),
+(6, 2, 6, 'martedi', 3, 10),
+(7, 2, 7, 'martedi', 3, 10),
+(8, 2, 1, 'giovedi', 3, 10),
+(9, 2, 2, 'giovedi', 3, 10),
+(10, 2, 3, 'sabato', 3, 10);
+
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `schede_palestra`
+-- Struttura della tabella `schede_utente`
 --
 
-DROP TABLE IF EXISTS `schede_palestra`;
-CREATE TABLE `schede_palestra` (
+CREATE TABLE `schede_utente` (
   `id` int(11) NOT NULL,
   `id_utente` int(11) DEFAULT NULL,
-  `id_allenatore` int(11) DEFAULT NULL
+  `id_scheda` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Svuota la tabella prima dell'inserimento `schede_palestra`
+-- Dump dei dati per la tabella `schede_utente`
 --
 
-TRUNCATE TABLE `schede_palestra`;
+INSERT INTO `schede_utente` (`id`, `id_utente`, `id_scheda`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
 -- --------------------------------------------------------
 
 --
 -- Struttura della tabella `utenti`
 --
 
-DROP TABLE IF EXISTS `utenti`;
 CREATE TABLE `utenti` (
   `id` int(11) NOT NULL,
   `username` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `ruolo` enum('user','admin') DEFAULT NULL,
-  `certificatoMedico` enum('approvato','non approvato','assente') DEFAULT NULL
+  `certificatoMedico` enum('approvato','non approvato','assente') DEFAULT NULL,
+  `nome` varchar(30) DEFAULT NULL,
+  `cognome` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Svuota la tabella prima dell'inserimento `utenti`
+-- Dump dei dati per la tabella `utenti`
 --
 
-TRUNCATE TABLE `utenti`;
+INSERT INTO `utenti` (`id`, `username`, `email`, `password`, `ruolo`, `certificatoMedico`, `nome`, `cognome`) VALUES
+(1, 'user', 'user@gmail.com', 'user', 'user', 'approvato', 'user', 'user');
+
 -- --------------------------------------------------------
 
 --
 -- Struttura della tabella `utenti_abbonamenti`
 --
 
-DROP TABLE IF EXISTS `utenti_abbonamenti`;
 CREATE TABLE `utenti_abbonamenti` (
   `id_utente` int(11) NOT NULL,
   `id_abbonamento` int(11) NOT NULL,
@@ -190,10 +255,13 @@ CREATE TABLE `utenti_abbonamenti` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Svuota la tabella prima dell'inserimento `utenti_abbonamenti`
+-- Dump dei dati per la tabella `utenti_abbonamenti`
 --
 
-TRUNCATE TABLE `utenti_abbonamenti`;
+INSERT INTO `utenti_abbonamenti` (`id_utente`, `id_abbonamento`, `data_stipula`) VALUES
+(1, 1, '2023-12-01'),
+(1, 2, '2024-01-01');
+
 --
 -- Indici per le tabelle scaricate
 --
@@ -230,19 +298,27 @@ ALTER TABLE `macchinari`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indici per le tabelle `scheda`
+--
+ALTER TABLE `scheda`
+  ADD PRIMARY KEY (`id_scheda`),
+  ADD KEY `id_allenatore` (`id_allenatore`);
+
+--
 -- Indici per le tabelle `schede_esercizi`
 --
 ALTER TABLE `schede_esercizi`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `id_scheda` (`id_scheda`),
   ADD KEY `id_esercizio` (`id_esercizio`);
 
 --
--- Indici per le tabelle `schede_palestra`
+-- Indici per le tabelle `schede_utente`
 --
-ALTER TABLE `schede_palestra`
+ALTER TABLE `schede_utente`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_utente` (`id_utente`),
-  ADD KEY `id_allenatore` (`id_allenatore`);
+  ADD KEY `id_scheda` (`id_scheda`);
 
 --
 -- Indici per le tabelle `utenti`
@@ -265,80 +341,72 @@ ALTER TABLE `utenti_abbonamenti`
 -- AUTO_INCREMENT per la tabella `abbonamenti`
 --
 ALTER TABLE `abbonamenti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT per la tabella `allenatori`
 --
 ALTER TABLE `allenatori`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `esercizi`
 --
 ALTER TABLE `esercizi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT per la tabella `gruppimuscolari`
 --
 ALTER TABLE `gruppimuscolari`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT per la tabella `macchinari`
 --
 ALTER TABLE `macchinari`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT per la tabella `schede_palestra`
+-- AUTO_INCREMENT per la tabella `scheda`
 --
-ALTER TABLE `schede_palestra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `scheda`
+  MODIFY `id_scheda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT per la tabella `schede_esercizi`
+--
+ALTER TABLE `schede_esercizi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT per la tabella `schede_utente`
+--
+ALTER TABLE `schede_utente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `utenti`
 --
 ALTER TABLE `utenti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Limiti per le tabelle scaricate
 --
 
 --
--- Limiti per la tabella `esercizi`
+-- Limiti per la tabella `scheda`
 --
-ALTER TABLE `esercizi`
-  ADD CONSTRAINT `esercizi_ibfk_1` FOREIGN KEY (`id_macchinario`) REFERENCES `macchinari` (`id`);
+ALTER TABLE `scheda`
+  ADD CONSTRAINT `scheda_ibfk_1` FOREIGN KEY (`id_allenatore`) REFERENCES `allenatori` (`id`);
 
 --
--- Limiti per la tabella `macchinari`
+-- Limiti per la tabella `schede_utente`
 --
-ALTER TABLE `macchinari`
-  ADD CONSTRAINT `macchinari_ibfk_1` FOREIGN KEY (`id`) REFERENCES `gruppimuscolari` (`ID`);
-
---
--- Limiti per la tabella `schede_esercizi`
---
-ALTER TABLE `schede_esercizi`
-  ADD CONSTRAINT `schede_esercizi_ibfk_1` FOREIGN KEY (`id_scheda`) REFERENCES `schede_palestra` (`id`),
-  ADD CONSTRAINT `schede_esercizi_ibfk_2` FOREIGN KEY (`id_esercizio`) REFERENCES `esercizi` (`id`);
-
---
--- Limiti per la tabella `schede_palestra`
---
-ALTER TABLE `schede_palestra`
-  ADD CONSTRAINT `schede_palestra_ibfk_1` FOREIGN KEY (`id_utente`) REFERENCES `utenti` (`id`),
-  ADD CONSTRAINT `schede_palestra_ibfk_2` FOREIGN KEY (`id_allenatore`) REFERENCES `allenatori` (`id`);
-
---
--- Limiti per la tabella `utenti_abbonamenti`
---
-ALTER TABLE `utenti_abbonamenti`
-  ADD CONSTRAINT `utenti_abbonamenti_ibfk_1` FOREIGN KEY (`id_utente`) REFERENCES `utenti` (`id`),
-  ADD CONSTRAINT `utenti_abbonamenti_ibfk_2` FOREIGN KEY (`id_abbonamento`) REFERENCES `abbonamenti` (`id`);
+ALTER TABLE `schede_utente`
+  ADD CONSTRAINT `schede_utente_ibfk_1` FOREIGN KEY (`id_scheda`) REFERENCES `scheda` (`id_scheda`),
+  ADD CONSTRAINT `schede_utente_ibfk_2` FOREIGN KEY (`id_utente`) REFERENCES `utenti` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
